50480589d35099c160c46151eb5cbc9f0afe2f8c
Paulo Clavijo Esteban <paucls@gmail.com>
2016-11-08 14:05:59 +0000

    Finished TaskApiController using a TasksStub service.

--- src/main/java/io/tasklist/api/TasksApiController.java
+++ src/main/java/io/tasklist/api/TasksApiController.java
@@ -1,8 +1,6 @@
 package io.tasklist.api;
 
-import io.tasklist.model.Task;
-
-import io.swagger.annotations.*;
+import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,57 +8,37 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
+import io.swagger.annotations.ApiParam;
+import io.tasklist.model.Task;
 
 @javax.annotation.Generated(value = "class io.swagger.codegen.languages.SpringCodegen", date = "2016-11-05T12:06:02.530Z")
 
 @Controller
 public class TasksApiController implements TasksApi {
 
-    private long idsCount = 3;
-    private List<Task> stubTasks = buildStubTasks();
+	public ResponseEntity<List<Task>> tasksGet() {
+		return new ResponseEntity<List<Task>>(TasksStub.list(), HttpStatus.OK);
+	}
 
-    private List<Task> buildStubTasks() {
-        ArrayList<Task> tasks = new ArrayList<>();
-        tasks.add(new Task("task-1", "Buy milk."));
-        tasks.add(new Task("task-2", "Wash the dishes."));
+	public ResponseEntity<Void> tasksIdDelete(
+			@ApiParam(value = "Task identifier", required = true) @PathVariable("id") String id) {
+		TasksStub.delete(id);
+		return new ResponseEntity<Void>(HttpStatus.OK);
+	}
 
-        return tasks;
-    }
+	public ResponseEntity<Void> tasksIdPost(
+			@ApiParam(value = "Task identifier", required = true) @PathVariable("id") String id,
+			@ApiParam(value = "Task to update", required = true) @RequestBody Task task
 
-    public ResponseEntity<List<Task>> tasksGet() {
-        return new ResponseEntity<List<Task>>(stubTasks, HttpStatus.OK);
-    }
+	) {
+		TasksStub.update(task);
+		return new ResponseEntity<Void>(HttpStatus.OK);
+	}
 
-    public ResponseEntity<Void> tasksIdDelete(
-            @ApiParam(value = "Task identifier", required = true) @PathVariable("id") String id
-    ) {
-        stubTasks = stubTasks.stream()
-                .filter(task -> !task.getId().equals(id))
-                .collect(Collectors.toList());
-
-        return new ResponseEntity<Void>(HttpStatus.OK);
-    }
-
-    public ResponseEntity<Void> tasksIdPost(
-            @ApiParam(value = "Task identifier", required = true) @PathVariable("id") String id,
-            @ApiParam(value = "Task to update", required = true) @RequestBody Task task
-
-    ) {
-        // do some magic!
-        return new ResponseEntity<Void>(HttpStatus.OK);
-    }
-
-    public ResponseEntity<Task> tasksPost(
-            @ApiParam(value = "Task to add to the list", required = true) @RequestBody Task task
-    ) {
-        task.setId("task-" + idsCount++);
-        stubTasks.add(task);
-
-        return new ResponseEntity<Task>(task, HttpStatus.OK);
-    }
+	public ResponseEntity<Task> tasksPost(
+			@ApiParam(value = "Task to add to the list", required = true) @RequestBody Task task) {
+		Task createdTask = TasksStub.create(task);
+		return new ResponseEntity<Task>(createdTask, HttpStatus.OK);
+	}
 
 }
