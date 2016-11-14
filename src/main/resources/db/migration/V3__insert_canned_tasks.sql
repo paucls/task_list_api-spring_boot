007d62688c03bc9281a97cffeb046c2093f5fb6c
Paulo Clavijo Esteban <paucls@gmail.com>
2016-11-14 22:28:21 +0000

    Fixed configuration of FlywayDB on application.properties. Added second script to insert canned tasks data.

---
+++ src/main/resources/db/migration/V3__insert_canned_tasks.sql
@@ -0,0 +1,4 @@
+INSERT INTO TASK (ID, NAME, DONE) VALUES ('task-0', 'Clean the car.', false);
+INSERT INTO TASK (ID, NAME, DONE) VALUES ('task-1', 'Clean the bathroom.', false);
+INSERT INTO TASK (ID, NAME, DONE) VALUES ('task-2', 'Buy milk.', false);
+INSERT INTO TASK (ID, NAME, DONE) VALUES ('task-3', 'Return books to the library.', true);
